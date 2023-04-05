0b61457c3c24fd2583dac100fe52c2cac70c5c7e
zverevs <129664415+zverevs@users.noreply.github.com>
2023-04-05 19:16:10 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,12 +10,12 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    n = len(x)  
-    alpha = 1 - p 
-    s2 = np.var(x, ddof=0)
-
-    chi2_alpha_2 = chi2.ppf(1 - alpha/2, df=2*n) 
-    chi2_1_minus_alpha_2 = chi2.ppf(alpha/2, df=2*n) 
-    left = np.sqrt((2 * n * s2) / chi2_alpha_2)
-    right = np.sqrt((2 * n * s2) / chi2_1_minus_alpha_2)
-    return left, right
+    n = len(x)
+    s = np.sqrt(np.sum(x**2) / n)
+    alpha = 1 - p
+    df = 2*n
+    chi2_left = chi2.ppf(alpha/2, df)
+    chi2_right = chi2.ppf(1 - alpha/2, df)
+    left_bound = np.sqrt((n-1)*s**2 / chi2_right)
+    right_bound = np.sqrt((n-1)*s**2 / chi2_left)
+    return (left_bound, right_bound)
